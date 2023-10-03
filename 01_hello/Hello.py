9bedd7f2f20b13cfb808dcda34ef92312f67ea63
Loupial <jeremy.boussaguet@protonmail.com>
2023-10-03 11:54:24 +0200

    Final Hello commit

--- 01_hello/Hello.py
+++ 01_hello/Hello.py
@@ -1,10 +1,32 @@
 #!/usr/bin/env python3
-# Purpose: Say hello
+"""
+Author: It's a me, MARIOOOOOOOOOOOO
+Purpose: Say hello
+"""
 
 import argparse
 
-parser = argparse.ArgumentParser(description='Say hello')
-parser.add_argument('-n', '--name', metavar='name', 
-                    default='World', help="It's the name of the person you want to greet")
-args = parser.parse_args()
-print('Hello, ' + args.name + '!')
+"-----------------------------------------------------------------------------------------"
+def get_args():
+    """
+    Get the command-line arguments
+    """
+    parser = argparse.ArgumentParser(description='Say hello')
+    parser.add_argument('-n', '--name', metavar='name',
+                        default='World',
+                        help="It's the name of the person you want to greet")
+    return parser.parse_args()
+
+
+"-----------------------------------------------------------------------------------------"
+def main():
+    """
+    Main program
+    """
+    args = get_args()
+    print('Hello, ' + args.name + '!')
+
+
+"-----------------------------------------------------------------------------------------"
+if __name__ == '__main__':
+    main()
